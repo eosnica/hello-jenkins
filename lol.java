b5b281a616061189b7abc3c53790d65e813da75d
eosnica <93702341+eosnica@users.noreply.github.com>
2021-11-04 10:13:39 +0100

    Create lol.java

---
+++ lol.java
@@ -0,0 +1,7 @@
+public class Principal 
+{
+	//fonction permettant de lancer le programme //<-- ceci est un commentaire
+	public static void main(String[] args) 
+
+system.out.println ("Hello World");
+}
