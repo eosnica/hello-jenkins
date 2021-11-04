a6fe9796ae34e873ecef6d09c68d6362c9883816
eosnica <93702341+eosnica@users.noreply.github.com>
2021-11-04 10:14:24 +0100

    Delete hello.java

--- hello.java
+++
@@ -1,7 +0,0 @@
-public class Principal 
-{
-	//fonction permettant de lancer le programme //<-- ceci est un commentaire
-	public static void main(String[] args) 
-
-system.out.println ("Hello World");
-}
